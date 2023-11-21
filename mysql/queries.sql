c88db970a068db1349aa2dfe12bf3325c59b86fc
Anshul Ranjan <ItsAnshulRanjan@gmail.com>
2023-11-21 21:29:46 +0530

    correlated queries done

--- mysql/queries.sql
+++ mysql/queries.sql
@@ -4,4 +4,17 @@
 SELECT Project_Name
 FROM Project p
 WHERE p.Project_End_Date < CURDATE()
-AND p.Project_Status <> 'Completed'
+AND p.Project_Status <> 'Completed';
+
+SELECT u.Name AS User_Name
+FROM User u
+WHERE (
+    SELECT COUNT(DISTINCT p.Project_ID)
+    FROM Project p
+    WHERE p.Project_Sprint_ID = u.Sprint_ID
+) > 1
+
+--Nested Queries
+
+
+--Aggregation Queries
